724e93354b0f0ba30faf00ee84c3961f7296459e
Eric Charnesky <eric.charnesky@careevolution.com>
2023-04-11 18:55:02 -0400

    first pass coffee shop with buttons

--- Week14-MoreGUI/src/main/java/com/example/week14moregui/HelloController.java
+++ Week14-MoreGUI/src/main/java/com/example/week14moregui/HelloController.java
@@ -2,10 +2,9 @@ package com.example.week14moregui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
 
 public class HelloController {
 
@@ -26,14 +25,49 @@ public class HelloController {
     @FXML
     private Label drinkLabel;
 
+    Beverage beverage;
+    @FXML
+    private ToggleGroup beverageType;
+
+    public HelloController(){
+        beverage = new Beverage("");
+    }
+
 
     @FXML
     public void milkCheckBoxChecked(ActionEvent actionEvent) {
+        setupBeverage();
+    }
+
+    private void setupBeverage(){
+        beverage = new Beverage("unknown");
+
+        if ( coffeeRadioButton.isSelected()){
+            beverage = new Beverage("Coffee");
+        } else if (espressoRadioButton.isSelected()){
+            beverage = new Beverage("Espresso");
+        } else if (teaRadioButton.isSelected()){
+            beverage = new Beverage("Tea");
+        }
+
+
+        if ( twoPercentMilkCheckBox.isSelected() ){
+            beverage.addMilk("2% milk");
+        }
+        if ( wholeMilkCheckBox.isSelected()){
+            beverage.addMilk("Whole milk");
+        }
+        if (soyMilkCheckBox.isSelected()){
+            beverage.addMilk("Soy milk");
+        }
+
+        drinkLabel.setText("$" + beverage.getCost());
     }
 
 
     @FXML
     public void beverageTypeRadioButtonClicked(ActionEvent actionEvent) {
+        setupBeverage();
     }
 
 }
